f19f5fa16a438f2d199a57f5e194a091584567a1
unknown <meontechno@gmail.com>
2019-03-06 00:29:01 -0800

    Successful convertion of h5 file to .pb and testing

--- architecture/mntiny.py
+++ architecture/mntiny.py
@@ -40,7 +40,7 @@ def _conv_block(inputs, filters, kernel, strides):
     channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
     x = Conv2D(filters, kernel, padding='same', strides=strides)(inputs)
-    #x = BatchNormalization(axis=channel_axis)(x)
+    # x = BatchNormalization(axis=channel_axis)(x)
     return ReLU(max_value=6)(x)
 
 
@@ -70,11 +70,11 @@ def _bottleneck(inputs, filters, kernel, t, s, r=False):
     x = _conv_block(inputs, tchannel, (1, 1), (1, 1))
 
     x = DepthwiseConv2D(kernel, strides=(s, s), depth_multiplier=1, padding='same')(x)
-    #x = BatchNormalization(axis=channel_axis)(x)
+    # x = BatchNormalization(axis=channel_axis)(x)
     x = ReLU(max_value=6)(x)
 
     x = Conv2D(filters, (1, 1), strides=(1, 1), padding='same')(x)
-    #x = BatchNormalization(axis=channel_axis)(x)
+    # x = BatchNormalization(axis=channel_axis)(x)
 
     if r:
         x = add([x, inputs])
